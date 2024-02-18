9e3834fc09b32a9a35e29befc0ac5316c75ae1d6
saumy85 <saumypandey7080@gmail.com>
2024-02-18 22:47:34 +0530

    1st change

--- backend/db/Product.js
+++ backend/db/Product.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema=new mongoose.Schema({
-    name:String,
-    price:String,
-    category:String,
-    userId:String,
-    company:String
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+  category: String,
+  userId: String,
+  company: String,
+  //fdhgd
 });
 
-module.exports=mongoose.model("products",productSchema);
+module.exports = mongoose.model("products", productSchema);
